1aac39766f3fce3bbcee8cd7bd521212f2773dde
adnaan512 <hassnain.bscs21seecs@seecs.edu.pk>
2025-10-05 16:06:19 +0500

    react context api

---
+++ minicontext/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './context/UserContextProvide'
+import Login from './components/Login'
+import Profile from './components/Profile'
+function App() {
+  
+
+  return (
+   <>
+   <UserContextProvider>
+   <h1>react with Adnan</h1>
+    <Login/>
+    <Profile/>
+   </UserContextProvider>
+   </>
+  )
+}
+
+export default App
